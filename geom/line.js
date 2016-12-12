b55d07578fafd316a2660a2ac0c9ec6217b5c4bb
Bharadwaj Kopparthi <pradeep146nanu@gmail.com>
2016-12-12 03:24:40 -0800

    Adding sample org structure dataset

--- geom/line.js
+++ geom/line.js
@@ -8,5 +8,35 @@ line={
         var obj= Object.create(this);
         obj.p1=p1;
         obj.p2=p2;
+        return obj;
+    },
+    drawLine:function(p1,p2){
+        if(p1 instanceof Point && p2 instanceof Point){
+            
+        }
+        if(p1 instanceof Array && p2 instanceof Array ){
+            if(p1.length== p2.length){
+                // Recursive funtion for creating multiple lines using p1 entry as starting point and p2 entry as ending point
+                a1=p1.slice();
+                a2=p2.slice();
+                return (function arrayLineBuilder(a1 ,a2){
+                    if(a1.length&& a2.length){
+                        console.log(a1, a2);
+                        var popp1=a1.pop();
+                        var popp2=a2.pop();
+                        console.log("pop 1 & 2");
+                        console.log(popp1, popp2)
+                        return "M "+popp1.x+" "+popp1.y+" L "+popp2.x+" "+popp2.y +" "+ arrayLineBuilder(a1,a2);
+                    }
+                    else{
+                        return "";
+                    }
+                })(a1,a2)
+            }
+        }
+        else if(p1 instanceof Array && !p2){
+            
+        }
+    
     }
 }
--- geom/point.js
+++ geom/point.js
@@ -1,10 +1,27 @@
-point={
-    x:0, y:0,
-    create:function(x,y){ 
-    var obj=Object.create(this);
-    obj.x=x;
-    obj.y=y;
-    return obj;
+point=  {
+create:function(p,q){
+    if(typeof p == "number" && typeof q == "number"){
+        var obj= Object.create(new Point);
+        obj.x=p;
+        obj.y=q;
+        return obj;
+    } 
+    else 
+    if(p instanceof Array &&  !!!q){
+        var arr=new Array();
+        thisObject=this;
+        p.forEach(function(a){
+            var obj =Object.create(new Point);
+            obj.x=a[0];
+            obj.y=a[1];
+            arr.push(obj);
+        })
+        return arr;
+    }
     }
     
+}
+
+Point=function(x,y){
+    this.x=x;this.y=y
 }
