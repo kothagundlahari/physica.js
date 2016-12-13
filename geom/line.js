b9c9b032ccef5fe73ffa412ab7a75e3a773bf3b3
Bharadwaj Kopparthi <pradeep146nanu@gmail.com>
2016-12-13 07:15:14 -0800

    Ajax library added

--- geom/line.js
+++ geom/line.js
@@ -12,21 +12,18 @@ line={
     },
     drawLine:function(p1,p2){
         if(p1 instanceof Point && p2 instanceof Point){
-            
+           return "M "+p1.x+" "+p1.y+" L "+p2.x+" "+p2.y; 
         }
         if(p1 instanceof Array && p2 instanceof Array ){
             if(p1.length== p2.length){
                 // Recursive funtion for creating multiple lines using p1 entry as starting point and p2 entry as ending point
-                a1=p1.slice();
-                a2=p2.slice();
-                return (function arrayLineBuilder(a1 ,a2){
+                let a1=p1.reverse().slice();
+                let a2=p2.reverse().slice();
+                return (function binaryArrayLineBuilder(a1 ,a2){
                     if(a1.length&& a2.length){
-                        console.log(a1, a2);
                         var popp1=a1.pop();
                         var popp2=a2.pop();
-                        console.log("pop 1 & 2");
-                        console.log(popp1, popp2)
-                        return "M "+popp1.x+" "+popp1.y+" L "+popp2.x+" "+popp2.y +" "+ arrayLineBuilder(a1,a2);
+                        return "M "+popp1.x+" "+popp1.y+" L "+popp2.x+" "+popp2.y +" "+ binaryArrayLineBuilder(a1,a2);
                     }
                     else{
                         return "";
@@ -34,8 +31,19 @@ line={
                 })(a1,a2)
             }
         }
+        // if p1 is single array item then iterate through it
+        // and build line
         else if(p1 instanceof Array && !p2){
-            
+            let a1=p1.reverse().slice()
+            return (function arrayLineBuilder(a1){
+                if(a1.length){
+                    let popp=a1.pop();
+                    return "M "+popp.x+" "+popp.y+" "+arrayLineBuilder(a1);
+                }
+                else{
+                    return "";
+                }
+            })(a1)
         }
     
     }
